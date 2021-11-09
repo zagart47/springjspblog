008ed64934e8f9404f7467859d9563b55cef1228
zagart47 <zagart47@gmail.com>
2021-11-09 16:05:41 +0500

    minor fixes

--- src/main/java/arturz/springjspblog/controllers/IndexController.java
+++ src/main/java/arturz/springjspblog/controllers/IndexController.java
@@ -2,17 +2,18 @@ package arturz.springjspblog.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
         return "index";
     }
     @GetMapping("/about")
-    public String about() {
+    public String about(Model model) {
         return "about";
     }
 
